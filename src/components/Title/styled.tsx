970c31d4b67ab365b07af890d745835bc1b479fc
van thanh <=>
2021-05-16 20:04:19 +0700

    Updates

--- src/components/Title/styled.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledTitle = styled.h1`
-  font-size: 18px;
-  margin: 0 0 30px;
-  color: #329252;
-  font-weight: bold;
-`;
--- src/styled.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledMainContent = styled.div`
-  padding: 50px 20px;
-  margin-left: 270px;
-  width: calc(100% - 270px);
-`;
