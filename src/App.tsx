cd24dc5c454fcbcbb937c0e2519fc8f4c0803891
van thanh <=>
2021-05-16 19:14:43 +0700

    Split the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import './App.css';
-import Counter from './components/counter';
-import EmployeeList from './components/employeeList';
+import Counter from './components/Counter/index';
+import EmployeeManagement from './components/EmployeeManagment/index';
+import SideBar from './components/SideBar/index';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import { ToastProvider } from 'react-toast-notifications';
+import { StyledMainContent } from './styled';
 
 function App() {
   return (
-    <div className="App">
-      <Counter />
-      <EmployeeList />
-    </div>
+    <ToastProvider
+      autoDismiss
+      autoDismissTimeout={1500}
+      placement="top-right"
+    >
+      <div>
+        <Router>
+          <div>
+            <SideBar />
+            <StyledMainContent>
+              <Switch>
+                <Route exact path="/">
+                  <Counter />
+                </Route>
+                <Route path="/employee-management">
+                  <EmployeeManagement />
+                </Route>
+              </Switch>
+            </StyledMainContent>
+          </div>
+        </Router>
+      </div>
+    </ToastProvider>
   );
 }
 
